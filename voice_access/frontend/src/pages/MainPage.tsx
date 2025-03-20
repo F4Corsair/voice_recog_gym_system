981c46a60ccfa8073287954fe17260fc2f4e2f91
sorocode <tudgus010707@naver.com>
2025-03-21 01:41:14 +0900

    💩 오류 방지용 프롭값 추가

--- voice_access/frontend/src/pages/MainPage.tsx
+++ voice_access/frontend/src/pages/MainPage.tsx
@@ -1,10 +1,19 @@
 import AudioRecorder from "@/components/audio/AudioRecorder";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const MainPage = () => {
+  // FIXME: 오류 방지를 위한 임시 핸들러
+  const handleAudioFile = () => {
+    console.log("handle audio file temp");
+  };
   return (
     <>
       <p>음성으로 사용자를 인증하는 메인 페이지</p>
-      <AudioRecorder />
+      <AudioRecorder onFileReady={handleAudioFile} />
+      <Button variant={"link"}>
+        <Link to="/signup">회원등록</Link>
+      </Button>
     </>
   );
 };
