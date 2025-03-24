1cc20150876cae9a9fe5f61b450a6d7a936beaa6
aa093094 <108619758+aa093094@users.noreply.github.com>
2025-03-24 20:27:19 +0900

    Update init.sql (users table create 문 추가)

--- admin_page/backend/init.sql
+++ admin_page/backend/init.sql
@@ -1,2 +1,18 @@
 -- CREATE USER 'user'@'%' IDENTIFIED BY 'password';
 -- GRANT ALL ON *.* TO 'user'@'%';
+
+-- create user table
+create table users(
+	order_id int auto_increment primary key,
+	name varchar(8) not null,
+	phone_num varchar(18) not null,
+	birth_date DATE not null,
+	gender varchar(10) not null,
+	height float,
+	weight float,
+	voice varchar(200),
+	unique (phone_num)
+);
+
+-- convert table to accept korean
+ALTER TABLE users CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
